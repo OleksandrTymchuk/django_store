449038dbf27066ccda8c83b744446349df732786
mrkorf <sashatimchuk2000@gmail.com>
2023-02-13 22:18:33 +0200

    add bootstrap navbar and css styles

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/django_project/static/',
+]
 
 STATIC_URL = 'static/'
 
