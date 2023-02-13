7369bf4932d4a548ef041d7c8bb4557c6fce407c
mrkorf <sashatimchuk2000@gmail.com>
2023-02-13 20:31:35 +0200

    add home page

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include("main.urls"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return render(request, 'main/index.html')
