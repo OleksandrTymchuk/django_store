2b7d83259727b31851b87a477650abdd798e7df4
mrkorf <sashatimchuk2000@gmail.com>
2023-02-14 02:14:59 +0200

    create model product and migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    description = models.TextField()
+    approved_by = models.ForeignKey(User, related_name="approved_by", null=True, on_delete=models.SET_NULL)
+    approved = models.BooleanField(default=False)
