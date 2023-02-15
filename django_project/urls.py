89eeae67aaba76313a74cc66dc92b0a7a5be1222
mrkorf <sashatimchuk2000@gmail.com>
2023-02-15 15:18:15 +0200

    init nginx and gunicorn

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("main.urls"))
+    path('', include("main.urls"))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
