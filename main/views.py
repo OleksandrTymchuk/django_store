11f237bfd225871bf38a9a915add07f06129503a
mrkorf <sashatimchuk2000@gmail.com>
2023-02-15 17:40:50 +0200

    add main menu product card

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MenuItem
+from products.models import Product
 
 def home(request):
     menu_items = MenuItem.objects.all()
-    return render(request, 'main/index.html', {"menu_items": menu_items})
+    products = Product.objects.filter(display_on_main_page=True, approved = True).order_by("id")
+    return render(request, 'main/index.html', {
+        "menu_items": menu_items,
+        "products":products
+    })
