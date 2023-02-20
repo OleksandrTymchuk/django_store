6a150b8bb1144f0b3f72b879a1a2c2f4c2c033ab
mrkorf <sashatimchuk2000@gmail.com>
2023-02-20 06:37:52 +0200

    add sign-in, sign-up and logout

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, sign_up, sign_in, logout_view
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("sign-up", sign_up, name="sign-up"),
+    path("sign-in", sign_in, name="sign-in"),
+    path("logout", logout_view, name="logout")
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import MenuItem
+from django.contrib.auth.models import User
 from products.models import Product
 
 def home(request):
     menu_items = MenuItem.objects.all()
     products = Product.objects.filter(display_on_main_page=True, approved = True).order_by("id")
+    print(request.user)
     return render(request, 'main/index.html', {
         "menu_items": menu_items,
         "products":products
     })
+
+def sign_up(request):
+    if request.method == "POST":
+        user = User()
+        user.email = request.POST.get("email")
+        user.username = request.POST.get("username")
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.set_password(request.POST.get("password"))
+        user.is_superuser = False
+        user.is_staff = False
+        user.is_active = True
+        user.save()
+        if user:
+            login(request, user)
+        return redirect("/")
+
+    else:
+        return render(request, "main/sign-up.html", {})
+
+def sign_in(request):
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST.get("username"), password=request.POST.get("password"))
+        print("AUTH")
+        print(user)
+        if user:
+            login(request, user)
+        return redirect("/")
+    else:
+        return render(request, "main/sign-in.html", {})
+
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect("/")
