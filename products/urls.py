3944de81703316fd345794d1914a51508461e2c3
mrkorf <sashatimchuk2000@gmail.com>
2023-02-21 10:23:53 +0200

    add "/" to urls

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import add_product, product_details
 
 urlpatterns = [
-    path("add", add_product, name="add_product"),
-    path("<int:id>", product_details, name='product_details')
-
+    path("/add", add_product, name="add_product"),
+    path("/<int:id>", product_details, name="product_details")
 ]
