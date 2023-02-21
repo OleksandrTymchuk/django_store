f4e48bfde137f0afc8f2914f411e1224ee14620e
mrkorf <sashatimchuk2000@gmail.com>
2023-02-21 16:13:50 +0200

    fix entrypoint to work correctly

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -1,1 +1,4 @@
-/django_project/docker/python/wait-for-it db:3306 -- python /django_project/manage.py migrate
+#!/bin/bash
+
+/django_project/docker/python/wait-for-it.sh db:3306 -- python /django_project/manage.py migrate
+gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
