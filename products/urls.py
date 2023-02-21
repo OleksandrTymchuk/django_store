10b479c41be1a5d84cc6c5c52b5e6b9d5546d582
mrkorf <sashatimchuk2000@gmail.com>
2023-02-21 21:11:35 +0200

    add category and category update pages

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import add_product, product_details, add_category
+from .views import add_product,\
+    product_details,\
+    add_category,\
+    category_page,\
+    update_product
 
 urlpatterns = [
     path("/add", add_product, name="add_product"),
+    path("/update/<int:id>", update_product, name="update_product"),
     path("/category/add", add_category, name="add_category"),
-    path("/<int:id>", product_details, name="product_details")
+    path("/<int:id>", product_details, name="product_details"),
+    path("/category/<str:slug>", category_page, name="category_page"),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
 from .forms import ProductForm
+from django.http import Http404
+
 
 def add_product(request):
     if request.user.is_authenticated:
@@ -24,9 +26,11 @@ def add_product(request):
     else:
         return redirect("/")
 
+
 def product_details(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, "products/details.html", {"product":product})
+
 
 def add_category(request):
     if request.user.is_authenticated and request.user.is_staff:
@@ -41,3 +45,30 @@ def add_category(request):
         else:
             categories = Category.objects.order_by("-id")
             return render(request, "products/category/add.html", {"categories":categories})
+
+
+def category_page(request, slug):
+    try:
+        category = Category.objects.get(slug=slug)
+    except Category.DoesNotExist:
+        raise Http404()
+    return render(request, "products/category_products.html", {"products":category.products.all})
+
+
+def update_product(request, id):
+    if request.user.is_authenticated:
+        product = Product.objects.get(id=id)
+        if product.user_id == request.user.id:
+            if request.method == "GET":
+                categories = Category.objects.order_by("-id")
+                return render(request, "products/update.html", {"product":product, "categories":categories})
+            else:
+                product.title = request.POST.get("title", "")
+                product.description = request.POST.get("description", "")
+                product.save()
+                if request.POST.getlist("categories", False):
+                     for category_id in request.POST.getlist("categories"):
+                        category = Category.objects.get(id=category_id)
+                        category.products.add(product)
+                return redirect("/")
+    return redirect("/")
