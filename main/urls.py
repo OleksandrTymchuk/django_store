cc71e8c7f6fbd37b411c24ad252d2015f13013ca
mrkorf <sashatimchuk2000@gmail.com>
2023-03-02 18:38:35 +0200

    create cart urls

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import home, sign_up, sign_in, logout_view
+from .views import \
+    home,\
+    sign_up,\
+    sign_in,\
+    logout_view,\
+    add_to_cart,\
+    cart
 
 urlpatterns = [
     path("", home, name="home"),
     path("sign-up", sign_up, name="sign-up"),
     path("sign-in", sign_in, name="sign-in"),
-    path("logout", logout_view, name="logout")
-
+    path("logout", logout_view, name="logout"),
+    path("add-to-cart/<int:product_id>", add_to_cart, name="add_to_cart"),
+    path("cart", cart, name="cart")
 ]
