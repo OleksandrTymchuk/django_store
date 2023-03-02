f1e96496336f2f530b99291f5309fb445ea9ce1c
mrkorf <sashatimchuk2000@gmail.com>
2023-03-02 18:39:09 +0200

    create add_to_cart and cart views

--- main/views.py
+++ main/views.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import Http404
 from .forms import UserSignUpForm, UserSignInForm
 from .models import MenuItem
-from django.contrib.auth.models import User
 from products.models import Product
+
 
 def home(request):
     menu_items = MenuItem.objects.all()
     products = Product.objects.filter(display_on_main_page=True, approved = True).order_by("id")
-    print(request.user)
     return render(request, 'main/index.html', {
         "menu_items": menu_items,
         "products":products
     })
+
 
 def sign_up(request):
     form = UserSignUpForm(request.POST or None)
@@ -24,6 +24,7 @@ def sign_up(request):
         login(request, user)
         return redirect("/")
     return render(request, "main/sign-up.html", {"form": form})
+
 
 def sign_in(request):
     form = UserSignInForm(request.POST or None)
@@ -43,3 +44,24 @@ def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
         return redirect("/")
+
+
+def add_to_cart(request, product_id):
+    if Product.objects.get(id=product_id):
+        if product_id in request.session.get("products", []):
+            return redirect("/")
+        if request.session.get("products", False):
+            request.session["products"].append(product_id)
+            request.session.modified = True
+        else:
+            request.session["products"] = []
+            request.session["products"].append(product_id)
+    else:
+        raise Http404()
+    return redirect("/")
+
+
+def cart(request):
+    if request.session.get("products", False):
+        products = Product.objects.filter(id__in=request.session.get("products"))
+        return render(request, "main/cart.html", {"products": products})
